f1d79c90394fb2a41e6a31ac0e963b23341409e1
gregoirehonvault <gregpro@orange.fr>
2023-06-07 11:23:42 +0200

    Compiler et exécuter grâce à Ant

--- BasicAntProject/src/n7/Main.java
+++ BasicAntProject/src/n7/Main.java
@@ -3,8 +3,25 @@ package n7;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Créer les trois segments
+		Point p1 = new Point(3, 2);
+		Point p2 = new Point(6, 9);
+		Point p3 = new Point(11, 4);
+		Segment s12 = new Segment(p1, p2);
+		Segment s23 = new Segment(p2, p3);
+		Segment s31 = new Segment(p3, p1);
 
+		// Créer le barycentre
+		double sx = p1.getX() + p2.getX() + p3.getX();
+		double sy = p1.getY() + p2.getY() + p3.getY();
+		Point barycentre = new Point(sx / 3, sy / 3);
+
+		// Afficher le schéma
+		System.out.println("Le schéma est composé de : ");
+		s12.afficher();		System.out.println();
+		s23.afficher();		System.out.println();
+		s31.afficher();		System.out.println();
+		barycentre.afficher();	System.out.println();
 	}
 
 }
