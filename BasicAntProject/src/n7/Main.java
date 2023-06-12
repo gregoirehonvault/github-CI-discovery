03d0b9843a991db18dbc484c3ba93e6ac88d3f61
gregoirehonvault <gregpro@orange.fr>
2023-06-12 17:04:32 +0200

    Tester merge n°2

--- BasicAntProject/src/n7/Main.java
+++ BasicAntProject/src/n7/Main.java
@@ -19,8 +19,8 @@ public class Main {
 
 		// Afficher le schéma
 		System.out.println("Le schéma est composé de : ");
-		s12.afficher();		System.out.println();
 		s31.afficher();		System.out.println();
+		s12.afficher();		System.out.println();
 		s23.afficher();		System.out.println();
 		barycentre.afficher();	System.out.println();
 	}
